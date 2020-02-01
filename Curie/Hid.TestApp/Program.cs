7c2b87ade0e50e4836d56cd204e02f7d80a3a6f1
aliaksandr.vinichuk <a.vinichuk@intetics.com>
2020-02-01 18:51:04 +0300

    Implement HID test app

---
+++ Curie/Hid.TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using DataHandling.Core.Entities;
+using DataHandling.Hid;
+using DataHandling.Hid.Settings;
+
+namespace Hid.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hidSettings = new HidManageSettings
+            {
+                DeviceVendorId = 1241,
+                DeviceProductId = 41042,
+                UpdatePeriod = TimeSpan.FromSeconds(10)
+            };
+
+            var dataProvider = new HidDataProvider(hidSettings);
+
+            dataProvider.NewReadout += Output;
+
+            dataProvider.Init();
+
+            new AutoResetEvent(false).WaitOne();
+        }
+
+        private static void Output(Co2Reading reading)
+        {
+            Console.WriteLine(reading.ToNiceString());
+        }
+    }
+}
